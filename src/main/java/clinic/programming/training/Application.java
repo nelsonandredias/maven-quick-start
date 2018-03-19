e6cbbc36539b0c70c8514bf79f98afc677a53007
nelsondias <srandrediaz@gmail.com>
2018-03-19 11:06:06 +0100

    add a testing java code to project

---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,19 @@
+package clinic.programming.training;
+
+public class Application {
+
+	public Application() {
+		System.out.println("Inside Application");
+	}
+	
+	//method main(): ALWAYS the APPLICATION entry point
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Starting Application");
+		
+		Application app = new Application();
+		
+	}
+
+}
